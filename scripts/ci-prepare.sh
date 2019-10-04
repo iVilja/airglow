fec877fb17b6828b57a0f184b839af45056bfbea
Vilja <i@vilja.me>
2019-10-04 22:24:01 +0000

    Deployment: Use gpg key downloaded from himota.

--- scripts/ci-prepare.sh
+++ scripts/ci-prepare.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-openssl aes-256-cbc -K $encrypted_ba2df4291512_key -iv $encrypted_ba2df4291512_iv \
-    -in ./secure/bot.gpg.enc -out ./secure/bot.gpg -d
-gpg --import ./secure/bot.gpg
+mkdir -p ./tmp
+wget https://himota.moon.moe/gpg-keys/commoon-bot.gpg.enc --quiet -O ./tmp/bot.gpg.enc
+openssl aes-256-cbc -K $commoon_bot_key -iv $commoon_bot_iv \
+    -in ./tmp/bot.gpg.enc -out ./tmp/bot.gpg -d
+gpg --import ./tmp/bot.gpg
 git config --global user.email "bot@moon.moe"
 git config --global user.name "Bot Commoon"
 git config --global commit.gpgsign true
